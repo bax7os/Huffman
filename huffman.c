47b5c85dae3c1e8f4b20395210192b34e492d3a6
bax7os <ana.moraes@ufms.br>
2024-05-07 17:50:05 -0400

    preenchendo tabela de frequencia

--- huffman.c
+++ huffman.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 #define MAX 256 // ASCII
 
+//inicializando tabela de frequencia com 0 
+void inicializa_tabela_frequencia(unsigned int tabela[]){
+    for (int i = 0; i < MAX; i++)
+        tabela[i] = 0;
+}
+
+void preenche_tabela_frequencia(unsigned char texto[], unsigned int tabela[]){
+    int compare = 0;
+    int i = 0;
+    while (texto[i] != '\0')
+    {
+        tabela[texto[i]]++;
+        i++;
+    }
+}
+
+void imprime_tabela_frequancia(unsigned int tabela[]){
+    for (int i = 0; i < MAX; i++)
+    {
+        printf("\t%d = %d = %c\n", i, tabela[i], i);
+    }
+    
+
+}
+
+
+
 int main(int argc, char *argv[]) {
+    
+    unsigned char linha[MAX];
+    unsigned int tabela_frequencia[MAX];
+
+
+    // -------------------------Leitura-do-Arquivo-----------------------------------
+
     FILE *arquivo;
-    char linha[MAX];
     char nome_arquivo[100]; // Tamanho máximo do nome do arquivo
+
 
     // Verifique se o nome do arquivo foi fornecido como argumento de linha de comando
     if (argc != 2) {
@@ -34,5 +68,10 @@ int main(int argc, char *argv[]) {
     // Feche o arquivo
     fclose(arquivo);
 
+
+    // -------------------------Tabela-de-frequencia-----------------------------------
+    inicializa_tabela_frequencia(tabela_frequencia);
+    preenche_tabela_frequencia(linha, tabela_frequencia);
+    //imprime_tabela_frequancia(tabela_frequencia);
     return 0;
 }
